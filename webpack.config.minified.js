1ee7539bc9f141a17dca693859b29633c9a2e5cb
rawls238 <garidor@bu.edu>
2015-08-18 19:24:53 -0400

    add minified version

---
+++ webpack.config.minified.js
@@ -0,0 +1,27 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './index.js',
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+    ]
+  },
+  output: {
+    filename: 'dist/react-experiments.min.js',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ],
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ]
+}
