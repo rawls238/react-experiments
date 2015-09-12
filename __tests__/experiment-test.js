c4c966b40a627e1b9973e2fb408573d63525bf3a
rawls238 <garidor@bu.edu>
2015-09-11 23:40:16 -0400

    add more tests

--- __tests__/experiment-test.js
+++ __tests__/experiment-test.js
@@ -14,23 +14,140 @@ describe('Test experiment component', () => {
       <ReactExperiments.Experiment param='foo' experimentClass={exp}>
         <ReactExperiments.Variation name='Variation A'>
           <span className='variation-a'>
-            foofuckingawesome
+            foo
           </span>
         </ReactExperiments.Variation>
         <ReactExperiments.Variation name='Variation B'>
           <span className='variation-b'>
-            foofuckingawesome
+            foo
           </span>
+        </ReactExperiments.Variation>
+      </ReactExperiments.Experiment>
+    );
+
+    //renders only one variation
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'experiment-variation-component'
+    ).length).toBe(1);
+
+    //renders the correct variation
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'variation-b'
+    ).length).toBe(1);
+  });
+
+  it('renders the default variation when needed', () => {
+    const experimentComponent = TestUtils.renderIntoDocument(
+      <ReactExperiments.Experiment experimentClass={exp}>
+        <ReactExperiments.Variation name='foo'>
+          foo
+        </ReactExperiments.Variation>
+        <ReactExperiments.Default>
+          <div className='default-div'>
+            Test
+          </div>
+        </ReactExperiments.Default>
+      </ReactExperiments.Experiment>
+    );
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'default-div'
+    ).length).toBe(1);
+  });
+
+  it('renders correctly when parameters are specified via the Variation component', () => {
+    const experimentComponent = TestUtils.renderIntoDocument(
+      <ReactExperiments.Experiment experimentClass={exp}>
+        <ReactExperiments.Variation param ='foo' name = 'Variation B'>
+          <div className='foo-variation'>
+            foo
+          </div>
+        </ReactExperiments.Variation>
+        <ReactExperiments.Default>
+          <div className='default-div'>
+            Test
+          </div>
+        </ReactExperiments.Default>
+      </ReactExperiments.Experiment>
+    );
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'foo-variation'
+    ).length).toBe(1);
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'default-div'
+    ).length).toBe(0);
+  });
+
+  it('works with nested variations', () => {
+    const experimentComponent = TestUtils.renderIntoDocument(
+      <ReactExperiments.Experiment experimentClass={exp}>
+        <ReactExperiments.Variation param ='foo' name = 'Variation B'>
+          <ReactExperiments.Variation param = 'test2' name='Num1'>
+            <div className='foo-variation'>
+              foo
+            </div>
+          </ReactExperiments.Variation>
+        </ReactExperiments.Variation>
+        <ReactExperiments.Default>
+          <div className='default-div'>
+            Test
+          </div>
+        </ReactExperiments.Default>
+      </ReactExperiments.Experiment>
+    );
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'foo-variation'
+    ).length).toBe(1);
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      'default-div'
+    ).length).toBe(0);
+  });
+
+  it('renders nothing with no defualt variation', () => {
+    const experimentComponent = TestUtils.renderIntoDocument(
+      <ReactExperiments.Experiment experimentClass={exp}>
+        <ReactExperiments.Variation param ='foobar' name = 'Variation B'>
+          <div className='foobar'>
+            test
+          </div>
         </ReactExperiments.Variation>
       </ReactExperiments.Experiment>
     );
     expect(TestUtils.scryRenderedDOMComponentsWithClass(
       experimentComponent,
-      'experiment-variation-component'
-    ).length).toBe(1);
+      'foobar'
+    ).length).toBe(0);
+  });
+
+  it('handles enrollment properly', () => {
+    const experimentComponent = TestUtils.renderIntoDocument(
+      <ReactExperiments.Experiment experimentClass={exp} shouldEnroll={false}>
+        <div className='foo'>
+          test
+        </div>
+      </ReactExperiments.Experiment>
+    );
     expect(TestUtils.scryRenderedDOMComponentsWithClass(
       experimentComponent,
-      'variation-b'
+      'foo'
+    ).length).toBe(0);
+
+    const experimentComponent2 = TestUtils.renderIntoDocument(
+      <ReactExperiments.Experiment experimentClass={exp} shouldEnroll={true}>
+        <div className='foo'>
+          test
+        </div>
+      </ReactExperiments.Experiment>
+    );
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent2,
+      'foo'
     ).length).toBe(1);
   });
 });
--- __tests__/parametrize-test.js
+++ __tests__/parametrize-test.js
@@ -27,14 +27,14 @@ describe('Test parametrize component', () => {
   });
 
   it('should work with parametrize using context', () => {
-      const DoublyNestedComponent = React.createClass({
+    const DoublyNestedComponent = React.createClass({
       contextTypes: {
         experimentParameters: React.PropTypes.object.isRequired,
       },
 
       render() {
         return (
-          <span className={this.context.experimentParameters.test2}>
+          <span className={this.context.experimentParameters.foo}>
             foo
           </span>
         );
@@ -48,7 +48,7 @@ describe('Test parametrize component', () => {
 
       render() {
         return (
-          <span className={this.context.experimentParameters.foo}>
+          <span className={this.context.experimentParameters.test2}>
             <DoublyNestedComponent />
           </span>
         );
@@ -66,9 +66,9 @@ describe('Test parametrize component', () => {
     });
 
     const parametrized = TestUtils.renderIntoDocument(
-    <ReactExperiments.Parametrize experimentClass={exp}>
-      <ParametrizedComponent />
-    </ReactExperiments.Parametrize>
+      <ReactExperiments.Parametrize experimentClass={exp}>
+        <ParametrizedComponent />
+      </ReactExperiments.Parametrize>
     );
     expect(TestUtils.scryRenderedDOMComponentsWithClass(
       parametrized,
@@ -76,7 +76,83 @@ describe('Test parametrize component', () => {
     ).length).toBe(1);
     expect(TestUtils.scryRenderedDOMComponentsWithClass(
       parametrized,
-      'Num 2'
+      'Num1'
     ).length).toBe(1);
+  });
+
+  it('should work correctly after re-rendering', () => {
+    const TestComponent = React.createClass({
+      contextTypes: {
+        experimentParameters: React.PropTypes.object.isRequired,
+      },
+
+      render() {
+        if (!this.props.test) {
+          return null;
+        }
+        return (
+          <div className={this.context.experimentParameters.foo}>
+            test2
+          </div>
+        );
+      }
+    });
+
+    const Parametrized = React.createClass({
+      render() {
+        return (
+          <ReactExperiments.Parametrize experimentClass={exp}>
+            <TestComponent test={this.props.test}/>
+          </ReactExperiments.Parametrize>
+        );
+      }
+    });
+
+    const RerenderingComponent = React.createClass({
+      getInitialState() {
+        return {
+          'test': false
+        }
+      },
+
+      toggleTest() {
+        this.setState({
+          test: !this.state.test
+        })
+      },
+
+      render() {
+        return (
+          <div>
+            <input type='button' className='input-button' onClick={this.toggleTest} />
+            <Parametrized test={this.state.test} />
+          </div>
+        );
+      }
+    });
+
+    const experimentComponent = TestUtils.renderIntoDocument(
+      <RerenderingComponent />
+    );
+
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      exp.get('foo')
+    ).length).toBe(0);
+
+    //click the button and the rendering component should have rendered
+    const button = TestUtils.scryRenderedDOMComponentsWithClass(experimentComponent, 'input-button')[0];
+    React.addons.TestUtils.Simulate.click(button.getDOMNode());
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      exp.get('foo')
+    ).length).toBe(1);
+
+    //click it again and it should be gone
+    React.addons.TestUtils.Simulate.click(button.getDOMNode());
+    expect(TestUtils.scryRenderedDOMComponentsWithClass(
+      experimentComponent,
+      exp.get('foo')
+    ).length).toBe(0);
   });
 });
